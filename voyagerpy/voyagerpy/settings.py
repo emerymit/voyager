7e9d34bf0c5e2ffb9d9ac979164cb7a90e0288df
emerymit <mitchemery@outlook.com>
2021-03-10 20:40:19 -0500

    fixing the breaking change

--- voyagerpy/voyagerpy/settings.py
+++ voyagerpy/voyagerpy/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'voyagerpy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': DIRS_PATH,
+        'DIRS': str(DIRS_PATH),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
